6c3016d96a4fa1c51259e6c0eef1c64498aba1be
Matthew Lerman <nerdpebble@gmail.com>
2022-06-06 11:44:32 -0400

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var clickedRow = null;
 function changeColor(row) {
     changeClicked(row);
     if(row.style.background == "" || row.style.background == "white") {
         row.style.background = "#91BAD6";
         document.getElementById("select").disabled = false;
+        clickedRow = row.id;
     }
     else {
         row.style.background = "white";
@@ -19,25 +21,15 @@ function changeClicked(row) {
     }
 }
 
-// function checkForClicked() {
-//     let clicked = false;
-//     let rows = document.getElementsByTagName("table")[0].rows;
-//     for(let i = 0; i < rows.length; i++) {
-//         if(rows[i].style.background != "white") {
-//             clicked = true;
-//         }
-//     }
-//     return clicked;
-// }
+function changePage() {
+    let loc = clickedRow + ".html"
+    window.location.href = loc;
+}
 
-// function selected() {
-//     if(checkForClicked()) {
-//         alert("Choose an intensity!");
-//     }
-// }
+
 
 window.onload = function() {
     document.querySelectorAll('#intensities tr').forEach(e => e.addEventListener("click", function(){changeColor(e);}));
-    document.getElementById('select').addEventListener("click", function(){selected()});
+    document.getElementById('select').addEventListener("click", function(){changePage()});
     document.getElementById("select").disabled = true;
 }
