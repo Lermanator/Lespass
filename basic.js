7f389124110cd588dfab9ca804ca59954152d1ad
Matthew Lerman <nerdpebble@gmail.com>
2022-06-07 10:01:27 -0400

    button enable/disable

--- basic.js
+++ basic.js
@@ -1,8 +1,19 @@
-function checkIfEmpty(cell) {
+function checkIfEmpty() {
+    let inputs = document.querySelectorAll('input');
+    
+    let notEmpty = true;
+    for(let i = 0; i < inputs.length; i++) {
+        console.log(inputs[i].value)
+        if(inputs[i].value.length == 0) {
+            notEmpty = false;
+        }
+    }
+    if(notEmpty) {
+        document.getElementById('add').disabled = false;
+    }
 }
 
 window.onload = function() {
-    document.querySelectorAll('#name').forEach(e => e.addEventListener("click", function(){checkIfEmpty(e);}));
-    document.querySelectorAll('#desc').forEach(e => e.addEventListener("click", function(){checkIfEmpty(e);}));
+    document.querySelectorAll('input').forEach(e => e.addEventListener("input", function(){checkIfEmpty();}));
     document.getElementById('add').disabled = true;
 }
