fc1ef04eab78fce3584b837cd64573348c91a13b
Matthew Lerman <nerdpebble@gmail.com>
2022-06-07 10:40:34 -0400

    saves local storage added bunch of stuff

--- basic.js
+++ basic.js
@@ -1,9 +1,10 @@
+tasks = {};
+
 function checkIfEmpty() {
-    let inputs = document.querySelectorAll('input');
+    const inputs = document.querySelectorAll('input');
     
     let notEmpty = true;
     for(let i = 0; i < inputs.length; i++) {
-        console.log(inputs[i].value)
         if(inputs[i].value.length == 0) {
             notEmpty = false;
         }
@@ -13,7 +14,49 @@ function checkIfEmpty() {
     }
 }
 
+function addTask() {
+    const inputs = document.querySelectorAll('input');
+    tasks[inputs[0].value] = inputs[1].value;
+    saveTasks();
+    renderRows();
+    inputs[0].value = "";
+    inputs[1].value = "";
+    document.getElementById('add').disabled = true;
+}
+
+function saveTasks() {
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderRows() {
+    tasks = JSON.parse(window.localStorage.getItem("tasks"));
+    if(tasks == null) {
+        tasks = {};
+    }
+    while (document.getElementById("tasks").firstChild) {
+        document.getElementById("tasks").removeChild(document.getElementById("tasks").firstChild);
+    }
+    for(let key in tasks) {
+        if(key) {
+            let tr = document.createElement("tr");
+            let name = document.createElement("th");
+
+            name.scope ="row";
+            name.innerHTML = key;
+
+            let desc = document.createElement("td");
+            desc.innerHTML = tasks[key];
+
+            tr.appendChild(name);
+            tr.appendChild(desc);
+            document.getElementById("tasks").appendChild(tr);
+        }
+    }
+}
+
 window.onload = function() {
+    renderRows();
     document.querySelectorAll('input').forEach(e => e.addEventListener("input", function(){checkIfEmpty();}));
+    document.getElementById('add').addEventListener("click", function(){addTask()});
     document.getElementById('add').disabled = true;
 }
